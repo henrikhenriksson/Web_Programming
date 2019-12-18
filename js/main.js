ac0a84890cd108e99bbf6bec9181f99eb70cc8f4
Henrik Henriksson <hehe0601@student.miun.se>
2019-12-18 10:00:32 +0100

    E. Created folder structure and relevant files.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+    File:       main.js
+    Author:     Henrik Henriksson
+    Student ID: hehe0601¨
+    Contact:    hehe0601@student.miun.se
+    Version:    1.0
+    Created:    2019-12-18
+*/
